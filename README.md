bdfc35913463c883e17570039d70ee7f84cc2875
grh-gitee <3494242908@qq.com>
2022-11-19 20:34:09 +0800

    docs: save note

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # [ruihuag](https://guanruihua.github.io/#/)
 
 ![](/__assets__/001.jpg)
+
+<!-- [![Build Status](https://github.com/atelier-anchor/smiley-sans/workflows/build/badge.svg)](https://github.com/atelier-anchor/smiley-sans/actions) -->
+<!-- [![GitHub release](https://img.shields.io/github/release/atelier-anchor/smiley-sans/all.svg)](https://github.com/atelier-anchor/smiley-sans/releases/latest) -->
--- config/script/modules.js
+++ config/script/modules.js
@@ -1,19 +1,24 @@
-const modulesDom = document.querySelector('.modules')
-modules.forEach(name => {
-	const itemDom = document.createElement('div')
-	const imgDom = document.createElement('img')
-	imgDom.setAttribute('src', `./${name}/index.jpg`)
-	imgDom.setAttribute('title', name)
-	imgDom.onerror = function () {
-		this.src = './__assets__/001.jpg'
-	}
-	imgDom.setAttribute('alt', 'logo')
-	const itemTitle = document.createElement('h3')
-	itemTitle.innerHTML = name
-	itemDom.appendChild(imgDom)
-	itemDom.appendChild(itemTitle)
-	itemDom.onclick = () => {
-		window.location.href = '/' + name + '/index.html'
-	}
-	modulesDom.appendChild(itemDom)
-})
+const modulesDom = document.querySelector('.modules-content')
+
+function buildModulesDom(dom) {
+	modules.forEach(name => {
+		const itemDom = document.createElement('div')
+		const imgDom = document.createElement('img')
+		imgDom.setAttribute('src', `./${name}/index.jpg`)
+		imgDom.setAttribute('title', name)
+		imgDom.onerror = function () {
+			this.src = './__assets__/001.jpg'
+		}
+		imgDom.setAttribute('alt', 'logo')
+		const itemTitle = document.createElement('h3')
+		itemTitle.innerHTML = name
+		itemDom.appendChild(imgDom)
+		itemDom.appendChild(itemTitle)
+		itemDom.onclick = () => {
+			window.location.href = '/' + name + '/index.html'
+		}
+		dom.appendChild(itemDom)
+	})
+}
+
+buildModulesDom(modulesDom)
