b94db5a7777f7bfaa0b077ca157fa26146fe9ded
grh-gitee <3494242908@qq.com>
2022-11-20 10:23:35 +0800

    docs: save note 2022-11-20 10:23:35

--- push.bat
+++ push.bat
@@ -1,7 +1,5 @@
 @echo off
 
-echo %date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2%
-
 git add .
 git commit -m "docs: save note %date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2%"
 git push
