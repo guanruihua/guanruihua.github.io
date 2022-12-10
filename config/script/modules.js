46c64aa121bfa516ccfa9ddc1d58c369d1afe89d
grh-gitee <3494242908@qq.com>
2022-12-10 21:20:24 +0800

    docs: save note 2022-12-10 21:20:24

--- config/script/modules.js
+++ config/script/modules.js
@@ -1,20 +1,37 @@
 const modulesDom = document.querySelector('.modules-content')
 
+
+
 function buildModulesDom(dom) {
+	const __map__ = {
+		'Back-End': ['BE', 'End'],
+		'CSS': ['CS', 'CSS'],
+		'Data': ['DA', 'Data'],
+		'Database': ['DB', 'base'],
+		'Front-End': ['FE', 'End'],
+		'HTML': ['HT', 'HTML'],
+		'JavaScript': ['JS', 'Script'],
+		'Knowledge-Reserve': ['KR','ledge'],
+		'OpenSource': ['OS', 'Source'],
+		'React': ['RE', 'React'],
+		'Tool': ['TO', 'Tool'],
+		'TypeScript': ['TS', 'Script'],
+		get: function(name, index){
+			if (this[name])
+				return this[name][index]
+			else
+				return name
+		}
+	}
+	console.log(modules)
 	modules.forEach(name => {
+		const _name = name.split('-').join('\n')
 		const itemDom = document.createElement('div')
-		itemDom.setAttribute('title', name)
-		// const imgDom = document.createElement('img')
-		// imgDom.setAttribute('src', `./${name}/index.jpg`)
-		// imgDom.setAttribute('title', name)
-		// imgDom.onerror = function () {
-		// 	this.src = './__assets__/001.jpg'
-		// }
-		// imgDom.setAttribute('alt', 'logo')
-		const itemTitle = document.createElement('h3')
-		itemTitle.innerHTML = name
-		// itemDom.appendChild(imgDom)
-		itemDom.appendChild(itemTitle)
+		itemDom.setAttribute('title', _name)
+		// itemDom.setAttribute('data-word', _name)
+		itemDom.setAttribute('data-word', __map__.get(name, 0))
+		itemDom.innerText = _name
+		// itemDom.innerText = __map__.get(name, 1)
 		itemDom.onclick = () => {
 			window.location.href = '/' + name + '/index.html'
 		}
