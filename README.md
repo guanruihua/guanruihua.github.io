e331070fdd276decfb893d79390d1faec97c9578
grh-gitee <3494242908@qq.com>
2022-12-18 14:16:34 +0800

    docs: save note 2022-12-18 14:16:34

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 ## plan
 
 - `file-system-hoc` 包 准备中
-- 将`abandonjs`类型判断相关方法分离出 `check-it-type` 包
+- 开源项目 测试覆盖率
 
-## action
+## log
+
+### 2022-12-17
+
+- 将`abandonjs`类型判断相关方法分离出 `check-it-type` 包, 并发布
+- 整理 出 `npm-util-template` 模板
--- config/_modules_.js
+++ config/_modules_.js
@@ -2,3 +2,9 @@ const modules =[
 	"Back-End","CSS","Data","Database","Front-End","HTML","JavaScript","Knowledge-Reserve","OpenSource","React","Tool",
 	"TypeScript"
 ]
+
+const packages = [
+	{ type: 'npm', name: 'abandonjs', url: 'https://www.npmjs.com/package/abandonjs', },
+	{ type: 'npm', name: 'unit-testing-js', url: 'https://www.npmjs.com/package/unit-testing-js', },
+	{ type: 'npm', name: 'fakingjs', url: 'https://www.npmjs.com/package/fakingjs', },
+]
