cc9a3b578612041c6d2bfffa19cfe548b76f745f
guanruihua <2116047353@qq.com>
2025-05-27 14:53:52 +0800

    feat: dev

--- src/views/tool/upload-preview/cache/index.tsx
+++ src/views/tool/upload-preview/cache/index.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
 import { openDB } from './open'
 
+const genId = () => {
+  let d = new Date().getTime()
+  const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+    /[xy]/g,
+    function (c) {
+      const r = (d + Math.random() * 16) % 16 | 0
+      d = Math.floor(d / 16)
+      return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16)
+    },
+  )
+  return uuid
+}
+
 export interface UseIndexedDBProps {
   /**
    * @description
@@ -24,19 +37,11 @@ export const useIndexedDB = (props?: UseIndexedDBProps) => {
   return {
     db,
     getKey() {
-      let d = new Date().getTime()
-      const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
-        /[xy]/g,
-        function (c) {
-          const r = (d + Math.random() * 16) % 16 | 0
-          d = Math.floor(d / 16)
-          return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16)
-        },
-      )
-      if(this.getStore()?.get(uuid)){
-
-        // 重新生成
-      }
+      const uuid = genId()
+      // if (this.getStore()?.get(uuid)?.result) {
+      //   // 重新生成
+      //   return this.getKey()
+      // }
       return uuid
     },
     async getAll() {
--- src/views/tool/upload-preview/index.tsx
+++ src/views/tool/upload-preview/index.tsx
@@ -1,54 +1,98 @@
 import React from 'react'
 import './index.less'
 import { useIndexedDB } from './cache'
-import { Button } from 'aurad'
+import { classNames } from 'harpe'
 
 export function UploadPreview() {
-  const db = useIndexedDB()
-  const ref = React.useRef<HTMLDivElement>(null)
+  const cache = useIndexedDB()
+  const ref = React.useRef<HTMLInputElement>(null)
+  const [view, setView] = React.useState<string>('')
+  const [imgList, setImgList] = React.useState<
+    { key: string; value: string }[]
+  >([])
 
-  const handleFile = (files: File[] = []) => {
-    for (let i = 0; i < files.length; i++) {
-      const file = files[i]
+  const getFile = async (
+    file: File,
+  ): Promise<{ key: string; value: string } | null> => {
+    return new Promise((resolve) => {
       const reader = new FileReader()
-      // 将图像文件转换为 Data URL
       reader.readAsDataURL(file)
-      const img = document.createElement('img')
-
-      // 当读取完成时，显示预览图像
       reader.onload = function () {
         if (reader.result) {
-          img.src = reader.result as any
-          db.add(reader.result)
-          ref.current?.appendChild(img)
+          const { key, value } = cache.add(reader.result)
+          resolve({ key, value })
+        } else {
+          resolve(null)
         }
       }
+    })
+  }
+
+  const handleFile = async (files: File[] = []) => {
+    const newImgList: { key: string; value: string }[] = []
+
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i]
+      const record = await getFile(file)
+      if (record) newImgList.push(record)
     }
+    setImgList([...imgList, ...newImgList])
   }
 
   React.useEffect(() => {
-    db.getAll().then((list: any[]) => {
-      list?.forEach(({ value }) => {
-        const img = document.createElement('img')
-        img.src = value
-        ref.current?.appendChild(img)
-      })
-    })
+    imgList.length === 0 && cache.getAll().then(setImgList)
   }, [ref.current])
 
   return (
     <div>
-      <div ref={ref} className="image-previews"></div>
-      <input
-        type="file"
-        name="fileToUpload"
-        id="fileToUpload"
-        multiple
-        onChange={(e) => {
-          const value = e.target.files || []
-          handleFile(value as File[])
-        }}
-      />
+      <div className="image-preview">
+        {imgList.map((item) => {
+          const { key, value } = item
+          return (
+            <div key={key} className="image-preview-item">
+              <div
+                className={classNames('image-preview-item-img', {
+                  view: key === view,
+                })}
+                onClick={() => key === view && setView('')}
+              >
+                <img src={value} />
+              </div>
+              <div className="control">
+                <div onClick={() => setView(key)}>VIEW</div>
+                <div
+                  className="del"
+                  onClick={() => {
+                    setImgList((list) => list.filter((_) => _.key !== key))
+                    cache.del(key)
+                  }}
+                >
+                  DEL
+                </div>
+              </div>
+            </div>
+          )
+        })}
+        <div
+          key="upload"
+          className="upload"        
+          onClick={() => {
+            ref.current?.click()
+          }}
+        >
+          <span>Add</span>
+          <input
+            ref={ref}
+            type="file"
+            multiple
+            style={{ display: 'none' }}
+            onChange={(e) => {
+              const value = e.target.files || []
+              handleFile(value as File[])
+            }}
+          />
+        </div>
+      </div>
     </div>
   )
 }
