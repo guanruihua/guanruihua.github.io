1dbe986ad214afb4cba45c5a7f8d623112731371
guanruihua <2116047353@qq.com>
2025-06-12 11:29:34 +0800

    feat: dev

---
+++ src/views/home/guide/index.tsx
@@ -0,0 +1,83 @@
+import { Div } from 'aurad'
+import React from 'react'
+import { useNavigate } from 'react-router'
+import { useEventListener } from './eventlistener'
+import './index.less'
+
+export interface GuideProps {
+  guide: {
+    name?: string
+    type?: string
+    next: string[][]
+  }[]
+  state?: {
+    selects?: string[]
+    [key: string]: any
+  }
+  [key: string]: any
+}
+
+export function Guide(props: GuideProps) {
+  const nav = useNavigate()
+
+  const { guide, state } = props
+  const { selects = [] } = state || {}
+  const colWidth = 450
+
+  const getNewColCount = () => {
+    const w = window.document.body.getBoundingClientRect().width
+    return Math.max(Math.floor(w / colWidth), 1)
+  }
+
+  const [colCount = 1] = useEventListener<number>(
+    'resize',
+    getNewColCount,
+    getNewColCount(),
+  )
+
+
+  const canRenderGuide = guide.filter((_) =>
+    _.type && selects.length > 0 ? selects.includes(_.type) : true,
+  )
+
+  const cols = new Array(colCount).fill('').map((_, i) => {
+    return canRenderGuide.filter((_, j) => j % colCount === i)
+  })
+  // console.log(cols, colCount, guide.length)
+
+  const onClick = (url: string) => {
+    if (url.indexOf('.') > -1)
+      window.open(url.indexOf('http') > -1 ? url : `https://${url}`, '_blank')
+    else nav(url)
+  }
+  return (
+    <div className="home-guide">
+      {cols.map((col, ci) => {
+        return (
+          <div key={ci} className="home-guide-col">
+            {col?.map((item: GuideProps['guide']['0'], i: number) => {
+              const { name, next, type = 'Own' } = item
+              return (
+                <Div className="guide-module" key={i}>
+                  {name && <div className="title">{name}</div>}
+                  {next?.map((child, j) => {
+                    const [name, url] = child
+                    return (
+                      <div
+                        key={j}
+                        className="guide-item"
+                        onClick={() => onClick(url)}
+                      >
+                        {name}
+                      </div>
+                    )
+                  })}
+                </Div>
+              )
+            })}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
